72ed7a8f3e1cfb43d911984ca50a28ee5b8f5021
Mario <mario2603@Ubuntu-Please.myguest.virtualbox.org>
2023-05-26 10:56:01 -0400

    Comments out important code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+// Immediately invoked function expression to encapsulate code and avoid coupling
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
+            document.querySelector(".active-btn").classList.remove("active-btn"); //remove active state from previously active button
+            this.classList.add("active-btn"); // Make the button that is clicked visually active
+            document.querySelector(".active").classList.remove("active"); //Hide previous active buttons section
+            document.getElementById(button.dataset.id).classList.add("active"); //get the value of the data-id attribute of the clicked button and add it to the active class to make section visibile
         })
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
+        document.body.classList.toggle("light-mode");// when theme btn is clicked toggle on/off light mode
     })
 })();
